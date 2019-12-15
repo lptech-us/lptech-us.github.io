a8cbd2d7e11b1ec42dba69b06827ca90a692d899
lptech-us <57578294+lptech-us@users.noreply.github.com>
2019-12-14 19:52:16 -0800

    languages

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,7 +127,7 @@
 
 		// Toggle.
 			$navPanelToggle = $(
-				'<a href="#navPanel" id="navPanelToggle">目录</a>'
+				'<a href="#navPanel" id="navPanelToggle"></a>'
 			)
 				.appendTo($wrapper);
 
